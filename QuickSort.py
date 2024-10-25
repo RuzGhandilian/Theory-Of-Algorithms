50c86bfe6501913bc39c812e44374b6baa214dc5
RuzGhandilian <159611923+RuzGhandilian@users.noreply.github.com>
2024-10-25 13:42:33 +0400

    updated

--- QuickSort.py
+++ QuickSort.py
@@ -3,11 +3,10 @@ def quick_sort(arr):
         return arr
     else:
         pivot = arr[0]
-        left = [x for x in arr[1:] if x <= pivot]
-        right = [x for x in arr[1:] if x > pivot]
+        left = [a for a in arr[1:] if a <= pivot]
+        right = [a for a in arr[1:] if a > pivot]
         return quick_sort(left) + [pivot] + quick_sort(right)
 
 
-x = [2, 5, 1, 7, 2, 4, 6, 7, 2, 4, 2, 4, 2, 4, 6, 8, 9, 9]
-y = quick_sort(x)
-print(y)
+x = [64, 25, 32, 12, 66, 78, 1, 6, 3, 2, 7, 8, 12, 22, 11]
+print(quick_sort(x))
